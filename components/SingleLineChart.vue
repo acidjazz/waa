05962443e616c4fbcee284587a97fc8152013027
kevin olson <acidjazz@gmail.com>
2017-06-08 21:31:20 -0700

    range fix :wrench:

--- components/SingleLineChart.vue
+++ components/SingleLineChart.vue
@@ -56,7 +56,7 @@ export default {
 
       for (let key in data) {
         if (key >= current) {
-          if (key === current || key === '2030' || (++i % 4)) {
+          if (key === current || key === '2030' || !(++i % 3)) {
             labels.push(key)
             if (index !== undefined) {
               datas.push(data[key][index])
