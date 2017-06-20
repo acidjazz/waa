5f4b05f16798144410fc57d5f3fec6de559ac133
kevin olson <acidjazz@gmail.com>
2017-06-20 11:15:40 -0700

    missing period

--- operations/aeonian.js
+++ operations/aeonian.js
@@ -224,7 +224,7 @@ exports.updateCloudFrontOrigin = (id, domain, environment, complete) => {
         delete updateParams.ETag
 
         let previous = updateParams.Origins.Items[0].DomainName.replace('.s3-website-us-east-1.amazonaws.com', '')
-        let current = domain.replace('s3-website-us-east-1.amazonaws.com', '')
+        let current = domain.replace('.s3-website-us-east-1.amazonaws.com', '')
 
         updateParams.Origins.Items[0].DomainName = domain
         cloudfront.updateDistribution(updateParams, (terror, tdata) => {
