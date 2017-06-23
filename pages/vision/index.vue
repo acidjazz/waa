e5a2d1bffcf335690cd1877b3d6300088886eaeb
kevin olson <acidjazz@gmail.com>
2017-06-22 18:30:38 -0700

    page creation :construction:,, clean up navigation organization :lipstick:

---
+++ pages/vision/index.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+.page.page_vision
+  Top(type="light")
+  Bottom
+</template>
+<style lang="stylus">
+@import '../../assets/stylus/vision.styl'
+</style>
+<script>
+import Top from '~/components/Top.vue'
+import Bottom from '~/components/Bottom.vue'
+export default { components: { Top, Bottom } }
+</script>
