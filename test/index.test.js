e71ccae2c3ebbf6c5bb5f8895cce5bf199b82f33
kevin olson <acidjazz@gmail.com>
2017-06-27 23:00:03 -0700

    more test alterations

--- test/index.test.js
+++ test/index.test.js
@@ -1,30 +1,37 @@
+
 import test from 'ava'
 import Nuxt from 'nuxt'
 import { resolve } from 'path'
 
-// keep the nuxt and server instnace so we can close 'em at the end
+// We keep the nuxt and server instance
+// So we can close them at the end of the test
 let nuxt = null
 let server = null
 
+// Init Nuxt.js and create a server listening on localhost:4000
 test.before('Init Nuxt.js', async t => {
   const rootDir = resolve(__dirname, '..')
   let config = {}
   try { config = require(resolve(rootDir, 'nuxt.config.js')) } catch (e) {}
-  config.rootDir = rootDir // projet folder
-  config.dev = false // prod build
+  config.rootDir = rootDir // project folder
+  config.dev = false // production build
   nuxt = new Nuxt(config)
   await nuxt.build()
   server = new nuxt.Server(nuxt)
   server.listen(4000, 'localhost')
+
 })
 
+// Example of testing only generated html
 test('Route / exits and render HTML', async t => {
   let context = {}
   const { html } = await nuxt.renderRoute('/', context)
   t.true(html.includes('<div class="page home">'))
 })
 
+// Close server and ask nuxt to stop listening to file changes
 test.after('Closing server and nuxt.js', t => {
   server.close()
   nuxt.close()
 })
+
