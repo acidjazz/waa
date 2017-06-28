05ca871a4c5b5d65fae37e4d452ba8123c994c38
kevin olson <acidjazz@gmail.com>
2017-06-28 00:13:41 -0700

    lets try making our mixin non-ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,7 +64,9 @@ module.exports = {
   modules: [
     '@nuxtjs/font-awesome',
   ],
-  plugins: [{src: '~plugins/ga.js', ssr: false}],
+  plugins: [
+    {src: '~plugins/ga.js', ssr: false},
+    {src: '~plugins/filter-mixin.js', ssr: false}],
   router: {
     scrollBehavior: function (to, from, savedPosition) {
       if (to.name.indexOf('data') !== -1 && from.name.indexOf('data') !== -1) {
