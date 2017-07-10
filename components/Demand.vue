df0966f379c1f20e383823304ce4d48f8c6032ab
kevin olson <acidjazz@gmail.com>
2017-07-10 02:08:11 -0700

    🐛 override og:url

--- components/Demand.vue
+++ components/Demand.vue
@@ -88,6 +88,7 @@ export default {
       title: this.title,
       meta: [
         { hid: 'description', name: 'description', content: this.description },
+        { hid: 'og:url', property: 'og:url', content: this.$route.path },
         { hid: 'og:title', property: 'og:title', content: this.title },
         { hid: 'og:description', property: 'og:description', content: this.description },
         { hid: 'twitter:title', name: 'twitter:title', content: this.title },
