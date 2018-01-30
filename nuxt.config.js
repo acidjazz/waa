ea26a4641367bf40f82ab22746da266f146a686e
kevin olson <acidjazz@gmail.com>
2018-01-29 20:29:06 -0600

    🔧 upgraded chart.js to fix the mysterious BarrierChart issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ module.exports = {
       // { innerHTML: "alert('hi');" },
 
       { src: "//cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js" },
-      { src: "//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.min.js" },
+      { src: "//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.bundle.min.js" },
       { src: "//cdnjs.cloudflare.com/ajax/libs/axios/0.16.2/axios.min.js" },
       { src: "//cdnjs.cloudflare.com/ajax/libs/js-cookie/2.1.4/js.cookie.min.js" },
     ],
