b3f84f96da7293d8cfafb35c10e4531be679905e
kevin olson <acidjazz@gmail.com>
2018-02-09 18:00:02 -0600

    🎨 about page assets

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -25,20 +25,21 @@ doctype
       VertBorder
       .copys
         .block.block-image(v-in-viewport)
-          img(src="/about/child.jpg")
-        .block(v-in-viewport)
-          .title The Team
-          .copy Researchers from Hoyt Advisory Services (HAS), comprised of Dr. Jeffrey D. Fisher (Vice President, Hoyt Advisory Services & Professor Emeritus, Indiana University), Dr. Norman G. Miller (Professor of Real Estate, University of San Diego), Paige Mueller (Whitegate Real Estate Advisors), and Michael Dinn (Dinn Focused Marketing).
+          img(src="/about/1.png")
         .block(v-in-viewport)
           .title Forecasted Demographic
           .copy Economic and demographic trend and forecast data was drawn from Moody’s Analytics® supplemented by other sources including U.S. Census Bureau, Federal Reserve and other forecast surveys such as the Wall Street Journal Economic Forecasting Survey and the Federal Reserve Bank of Philadelphia Survey of Professional Forecasters. Property market data was derived from several sources including the U.S. Census Bureau, CoStar® Realty Information, CBRE® Econometrics and ESRI®.
         .block(v-in-viewport)
-          .title Current and Historical Demographic
+          .title Other Demographics
           .copy Descriptive current and historical demographic and housing data (such as the number of apartment residents and the size of the apartment stock) are NMHC tabulations of the most recently available American Community Survey microdata.
         .block(v-in-viewport)
           .title Economic Impact Data
           .copy Economic impact data (spending, economic contribution, and jobs created) were estimated by Dr. Stephen Fuller of George Mason University’s Center for Regional Analysis for 2013. 
+        .block(v-in-viewport)
+          .title The Team
+          .copy Researchers from Hoyt Advisory Services (HAS), comprised of Dr. Jeffrey D. Fisher (Vice President, Hoyt Advisory Services & Professor Emeritus, Indiana University), Dr. Norman G. Miller (Professor of Real Estate, University of San Diego), Paige Mueller (Whitegate Real Estate Advisors), and Michael Dinn (Dinn Focused Marketing).
         .clear
+    DiagSection
     .section.section_sources
       .inner
         .homes(v-in-viewport)
