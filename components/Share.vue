fdead3adb336dbc0cd915f054ec0a35eb3a93a67
kevin olson <acidjazz@gmail.com>
2018-03-28 18:39:16 -0500

    🔧 url back to PDF

--- components/Share.vue
+++ components/Share.vue
@@ -85,7 +85,7 @@ export default {
 
   data () {
     return {
-      basePDF: 'https://url-to-pdf-api.herokuapp.com/api/render?emulateScreenMedia=false&url=https://weareapartments.org',
+      basePDF: 'http://pdf.weareapartments.org/api/render?emulateScreenMedia=false&url=https://weareapartments.org',
       parsed: this.$route.hash.replace('#', '?'),
       modals: {
         shares: false
