0c8fda5044d64dc718f52e7ce4687e171e60190a
acidjazz <acidjazz@gmail.com>
2019-06-05 01:51:54 -0500

    🚧 finally a clean successful boilerplate

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,64 +1,95 @@
 import pkg from './package'
 
+let config = {
+  title: 'We Are Apartments: Learn about the demand for apartments in your area',
+  description: 'America needs to build more than 4.6 million new apartments by 2030 through bold, innovative solutions that will help meet America’s housing shortfall.',
+  url: 'https://weareapartments.org',
+  image: '/share.jpg',
+  keywords: 'affordable, data, apartments, census, calculator'
+}
+
 export default {
   mode: 'universal',
-
   /*
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: config.title,
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description },
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: '/bulma.css' },
-      { rel: 'stylesheet', href: '/mdi.css' },
-    ]
-  },
+      { name: 'viewport', content: 'user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, minimal-ui' },
+      { hid: 'description', name: 'description', content: config.description },
+      { name: 'msapplication-config', content: '/icons/browserconfig.xml' },
+      { name: 'theme-color', content: '#2f3e5d' },
 
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE: Edge, chrome: 1' },
+      { 'http-equiv': 'Content-Type', content: 'text/html; charset: UTF-8' },
+
+      // facebook
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: config.url },
+      { hid: 'og:image', property: 'og:image', content: config.url + config.image },
+
+      { hid: 'og:title', property: 'og:title', content: config.title },
+      { hid: 'og:description', property: 'og:description', content: config.description },
+
+      // twitter
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:image', content: config.url + config.image },
+
+      { hid: 'twitter:title', name: 'twitter:title', content: config.title },
+      { hid: 'twitter:description', name: 'twitter:description', content: config.description },
+
+    ],
+
+    link: [
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Rubik:100,200,300,400,500,700,900' },
+      { rel: 'stylesheet', href: '/mdi.css' },
+
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/icons/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icons/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icons/favicon-16x16.png' },
+      { rel: 'manifest', href: '/icons/manifest.json' },
+      { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#2f3e5d' },
+      { rel: 'shortcut icon', href: '/icons/favicon.ico' }
+    ],
+  },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
-
+  loading: { color: '#4da1ff' },
   /*
   ** Global CSS
   */
   css: [
     '@/assets/css/tailwind.css',
     '@/assets/stylus/main.styl',
+    '@/static/bulma.css',
+    // '@/static/mdi.css',
   ],
-
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
   ],
-
   /*
   ** Nuxt.js modules
   */
   modules: [
+    ['@nuxtjs/google-tag-manager', { id: 'GTM-NP9HJPR', pageTracking: false }],
+    'nuxt-purgecss',
   ],
-
   /*
   ** Build configuration
   */
   build: {
+    extractCSS: true,
     postcss: {
       plugins: [
         require('tailwindcss'),
-        require('autoprefixer')
+        require('autoprefixer'),
       ],
     },
-
-    /*
-    ** You can extend webpack config here
-    */
     extend(config, ctx) {
     }
   }
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,19 @@
 <template lang="pug">
-.container
-  .box
-    .bob hello
+section.section
+  .container
+    .box
+      .buttons
+        .button.is-primary
+          span button
+          span.icon: i.mdi.mdi-close
+        .button.is-info
+          span.icon: i.mdi.mdi-account
+          span button
+        .button.is-danger
+          span.icon: i.mdi.mdi-battery-charging
+          span button
+    section.section
+      .text-white.bg-red-500.p-2 tailwind stuff
+    section.section
+      .tailwind-bulma tailwind stuff from bulma
 </template>
-
-<script>
-export default {
-}
-</script>
