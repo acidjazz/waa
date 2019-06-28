a9d3c6424d503597b95624a76a766490806374a5
acidjazz <acidjazz@gmail.com>
2019-06-27 22:30:30 -0500

    📱 OurInitiative mobile fixes

--- components/home/OurInitiative.vue
+++ components/home/OurInitiative.vue
@@ -1,14 +1,16 @@
 <template lang="pug">
 #OurInitiative
   OffsetSection
-    .flex.flex-col.lg_flex-row(v-in-vp).vp-r.vp-d-3
+    .flex.flex-col.lg_flex-row.my-10.lg_my-0(v-in-vp).vp-r.vp-d-3
       img(src="/building.jpg")
-      .ml-10.flex.items-center(v-in-vp).vp-r.vp-d-4
+      .lg_ml-10.mt-10.lg_mt-0.flex.items-center(v-in-vp).vp-r.vp-d-4
         .copy
           .font-os.mb-4 CAMPAIGN
           .text-3xl.font-bold.mb-4 Our Initiative
           .text-xl.font-bold.mb-4 Growing apartment demand
           .text-black Delayed marriages, an aging population and immigration are creating a need for new apartments, to the tune of 4.6 million by 2030.
+
+
   .text-white.text-center.text-xs
     span Source:
     span ;&nbsp;
