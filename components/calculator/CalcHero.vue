c6532bb09bfe0963472b532df45859ae8a915b78
acidjazz <acidjazz@gmail.com>
2019-07-03 15:06:16 -0500

    🚧 calculator progress - matching desktop design

--- components/calculator/CalcHero.vue
+++ components/calculator/CalcHero.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 SectionHero(type="image",src="/highrises.jpg",gradient="bg-tranpurp")
-  .text-sl.font-os.mb-10(v-in-vp).vp-y CALCULATOR
+  .text-lg.font-os.mb-10(v-in-vp).vp-y CALCULATOR
   .mx-auto.text-4xl.w-10_12.lg_w-192.mb-10(v-in-vp).vp-y.vp-d-2 Calculate the economic contribution of an apartment community nationally, in your state or metro area below.
 </template>
 
