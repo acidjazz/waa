257fce3486f04c63b1e362c160bf9887fda01d18
acidjazz <acidjazz@gmail.com>
2019-07-23 02:21:51 -0500

    📱 more responsive tweaks

--- components/data/DataSummary.vue
+++ components/data/DataSummary.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.mt-32.pt-16.bg-trantrue.text-center.text-white.pb-64.-mb-48
+.mt-24.pt-24.bg-trantrue.text-center.text-white.pb-64.-mb-48
   .text-5xl.mb-4.font-bolder.ani-sit {{ area.location }}
   .lg_w-128.text-lg.mx-auto.ani-sit.ani-d-1.mb-8.px-2
     | {{ area.location }} apartments and their residents contribute
