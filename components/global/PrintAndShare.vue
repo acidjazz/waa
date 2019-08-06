407b54fb51ee6b61af7137b84e94d11b2eeeda45
acidjazz <acidjazz@gmail.com>
2019-08-06 12:00:34 -0500

    🔧 share functionality fixes

--- components/global/PrintAndShare.vue
+++ components/global/PrintAndShare.vue
@@ -6,8 +6,8 @@
     .text-sm.hover_underline.hidden.lg_block Printable PDF
   .flex.flex-col.items-center.justify-between
     .flex
-      a.mdi.p-1.lg_p-2.mdi-24px.mdi-facebook-box.hover_text-orange(href="test")
-      a.mdi.p-1.lg_p-2.mdi-24px.mdi-twitter.hover_text-orange(href="test")
+      a.mdi.p-1.lg_p-2.mdi-24px.mdi-facebook-box.hover_text-orange(@click="share('facebook')")
+      a.mdi.p-1.lg_p-2.mdi-24px.mdi-twitter.hover_text-orange(@click="share('twitter')")
     .text-sm.hidden.lg_block Share link
 </template>
 
@@ -28,5 +28,41 @@ export default {
       }).map(([k,v]) => `${k}=${v}`).join('&')
     },
   },
+  methods: {
+    share (type) {
+      let href = location.href
+      if (this.query) {
+        href = href.replace('#', '?')
+      }
+
+      if (type === 'facebook') {
+        this.popup('https://www.facebook.com/sharer/sharer.php?u=' + href,
+          'Share on Facebook', 626, 438)
+        return true
+      }
+      if (type === 'twitter') {
+        this.popup('https://twitter.com/intent/tweet?url=' + encodeURIComponent(href),
+          'Share on Twitter', 626, 438)
+        return true
+      }
+    },
+    popup (url, title, w, h) {
+      let dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left
+      let dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top
+
+      let width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width
+      let height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height
+
+      let left = ((width / 2) - (w / 2)) + dualScreenLeft
+      let top = ((height / 2) - (h / 2)) + dualScreenTop
+      let newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left)
+
+      // Puts focus on the newWindow
+      if (window.focus) {
+        newWindow.focus()
+      }
+    }
+
+  }
 }
 </script>
