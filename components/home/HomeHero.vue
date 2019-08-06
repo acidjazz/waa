1bf707f3076e309d32ac705cdf0abac213ec88a9
acidjazz <acidjazz@gmail.com>
2019-08-06 15:37:04 -0500

    💬 front page copy

--- components/home/HomeHero.vue
+++ components/home/HomeHero.vue
@@ -1,12 +1,18 @@
 <template lang="pug">
 SectionHero(type="video", src="/condos.mp4", gradient="bg-tranblue")
   .text-xl.font-os.mb-10(v-in-vp).vp-y WE ARE APARTMENTS.
-  .lg_text-5xl.text-2xl.px-4.lg_px-4.lg_w-256(v-in-vp).vp-y.vp-d-2 The country needs
-    span &nbsp;
+  .lg_text-5xl.text-2xl.px-4.lg_px-4.lg_w-256(v-in-vp).vp-y.vp-d-2
+    | Apartments and their residents contribute
+    | &nbsp;$
     no-ssr
-      VueCountUp(:endVal="count_val(endVal)",:options="options")
-    span &nbsp;
-    span million new apartments by 2030.  How many are needed near you?
+      VueCountUp(:endVal="count_val(contrib.endVal)",:options="contrib.options")
+    | &nbsp;
+    | trillion to the U.S. economy annually, supporting
+    | &nbsp;
+    no-ssr
+      VueCountUp(:endVal="count_val(contrib.endVal)",:options="contrib.options")
+    | &nbsp;
+    | million jobs.
   .buttons.mt-10
     nuxt-link(to="/vision",v-in-vp).waa-button.vp-z.mr-4.vp-d-3 Our vision
     nuxt-link(to="/data",v-in-vp).waa-button.vp-z.vp-d-4 The data
@@ -23,11 +29,20 @@ export default {
   mixins: [ count ],
   data () {
     return {
-      endVal: 4.6,
-      options: {
-        decimalPlaces: 1,
-        duration: 4,
-      }
+      contrib: {
+        endVal: 3.4,
+        options: {
+          decimalPlaces: 1,
+          duration: 4,
+        }
+      },
+      jobs: {
+        endVal: 17.5,
+        options: {
+          decimalPlaces: 1,
+          duration: 4,
+        }
+      },
     }
   }
 }
