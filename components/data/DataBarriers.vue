c3bf1d270cf446f9127a53f18bd1112b7c24f715
acidjazz <acidjazz@gmail.com>
2019-08-09 13:49:20 -0500

    ✨ RestrictionIndex chart component

--- components/data/DataBarriers.vue
+++ components/data/DataBarriers.vue
@@ -6,12 +6,26 @@
       .text-bolder.text-4xl Barriers to new apartments
       .text-2xl.font-bold.my-4 Multifamily supply restrictions index
       .text Based on specific factors like local regulations and available land to develop, the Barriers to Apartment Construction Index ranks 50 metro areas on how hard it is to build new apartments. See how your city stacks up.
+      RestrictionIndex.mt-4(
+        v-if="area.type === 'metro'",
+        :metro="cmetro",
+        :color="color(cmetro.Restriction_Index)")
     .w-full.lg_w-2_3.mt-8.lg_mt-0
+      .flex.justify-between.mb-4
+        span
+          i.mdi.mdi-circle.mx-4.text-bpink
+          span.text-alum Difficult Entry Point
+        span
+          i.mdi.mdi-circle.mx-4.text-orange
+          span.text-alum Moderate Entry Point
+        span
+          i.mdi.mdi-circle.mx-4.text-mountainmeadow
+          span.text-alum Easier Entry Point
       .flex.flex-row.flex-wrap
         .flex.w-1_2.lg_w-1_4(v-for="metro in sorted")
           .text-bolder.text-right.w-8.mr-4.tran-text-4s(:class="`text-${color(val(metro.Restriction_Index))}`")
             no-ssr
-              VueCountUp(:endVal="val(metro.Restriction_Index)")
+              VueCountUp(:endVal="val(metro.Restriction_Index)",:options="{decimalPlaces: 1}")
           .metro {{ metro.Metro_Area }}
 
 </template>
@@ -21,8 +35,9 @@
 import data from '@/mixins/data'
 import inViewport from 'vue-in-viewport-mixin'
 import DataName from '@/components/data/DataName'
+import RestrictionIndex from '@/components/data/RestrictionIndex'
 export default {
-  components: { DataName },
+  components: { DataName, RestrictionIndex },
   mixins: [ data, inViewport ],
   data () {
     return {
@@ -46,7 +61,10 @@ export default {
       return Object
         .values(this.restrictionMetro)
         .sort((a, b) => a.Restriction_Index <= b.Restriction_Index ? 1 : -1)
-    }
+    },
+    cmetro () {
+      return this.restrictionMetro[this.area.key]
+    },
   },
   watch: {
     'inViewport.now' (visible) {
@@ -58,6 +76,5 @@ export default {
     color (value) { return this.colors.find(c => value >= c.min).name },
     val (value) { return  this.fire ? value : 0 },
   },
-
 }
 </script>
---
+++ components/data/RestrictionIndex.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+#RestrictionIndex
+  .flex.items-center
+    .chart
+      .w-32.h-32.rounded-full.bg-rainbow.p-3.tran-all-children
+        .w-full.h-full.bg-white.rounded-full.p-3
+          .relative.w-full.h-full(:style="`transform: rotate(${rotation}deg);`")
+            .border-4.w-full.h-full.rounded-full.flex.items-center.justify-center(:class="`border-${color}`")
+              .pointer.top-0.absolute.-mt-2(:class="`pointer-${color}`")
+              .text-3xl.text-bolder(:style="`transform: rotate(${-rotation}deg);`") {{ index }}
+      .w-32.flex.text-alum.bg-white.justify-between.-mt-6
+        span Harder
+        span Easier
+    .ml-4.text-bolder
+      div {{ metro.Metro_Area }}'s
+      div Restriction Index
+</template>
+
+
+<script>
+export default {
+  props: {
+    metro: {
+      type: Object,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    }
+  },
+  data () {
+    return {
+      a: 20,
+      b: -5,
+      c: -130,
+      d: 130,
+    }
+  },
+  computed: {
+    index () { return this.metro.Restriction_Index },
+    rotation () {
+      return ( (this.index-this.a) * ( (this.d-this.c) / (this.b-this.a) ) ) + this.c
+    },
+  },
+  mounted () {
+    console.log(this.metro)
+  },
+}
+</script>
