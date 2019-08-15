a7054db2fcc1e076cf621004285f7134574195b4
acidjazz <acidjazz@gmail.com>
2019-08-15 18:16:27 -0500

    📱 potential spacing print fix take 4

--- components/data/DataDetailHomes.vue
+++ components/data/DataDetailHomes.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 #DataDetailsHomes.lg_h-64.print_h-48.flex.flex-col
-  .text-bolder.text-6xl.hidden.sm_block.md_block.lg_block: VueCountUp(:endVal="count_val(apartments)",:options="count_opts(apartments)")
-  .text-bolder.text-3xl.lg_hidden.sm_hidden.md_hidden {{ apartments | numeral }}
+  .text-bolder.text-6xl.print_hidden: VueCountUp(:endVal="count_val(apartments)",:options="count_opts(apartments)")
+  .text-bolder.text-3xl.hidden.print_block {{ apartments | numeral }}
   .text-bolder.text-2xl Apartment Homes
   .my-6.lg_mt-6.print_mt-2.print_my-2.print_text-sm
     | The operation of {{ loc_copy }}'s apartment homes contributes
