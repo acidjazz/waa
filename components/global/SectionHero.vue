e9ba345374e0438b5f4e9ab412397bf903681d4f
acidjazz <acidjazz@gmail.com>
2019-08-15 18:49:02 -0500

    🔧 edge video fix take 2

--- components/global/SectionHero.vue
+++ components/global/SectionHero.vue
@@ -5,9 +5,9 @@
     playsinline,
     autoplay="autoplay",
     muted="muted",
-    loop="loop").absolute.w-full.h-full.top-50.left-50.w-auto.h-auto.transform-50.object-cover.z-5
+    loop="loop").absolute.w-screen.h-screen-bl48.top-50.left-50.w-auto.h-auto.transform-50.object-cover.z-5
     source(:src="src",type="video/mp4")
-  img(v-if="type === 'image'",:src="src",alt="Hero Image").object-cover.absolute.w-full.h-full
+  img(v-if="type === 'image'",:src="src",alt="Hero Image").object-cover.absolute.w-screen.h-screen-bl48
   .relative.w-screen.h-screen-bl48.flex.justify-center.items-center(:class="gradient")
     .text-white.text-center
       slot
