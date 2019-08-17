39d5eb42f68654b45beb0dccafc1f2814fa14473
acidjazz <acidjazz@gmail.com>
2019-08-16 19:36:49 -0500

    📱 ie11 page take two

--- layouts/compat.vue
+++ layouts/compat.vue
@@ -1,6 +1,13 @@
 <template lang="pug">
-#Incompatible.text-white.w-screen.h-screen.flex.items-center.justify-center
-  .text-2xl Your browser is not compatible, please upgrade to the latest Chrome, Safari, or FireFox
+#Incompatible.bg-tranpurp.text-white.w-screen.h-screen.flex.items-center.justify-center
+  .text-2xl.w-128.text-center
+    | Your browser is not compatible, please upgrade to the latest
+    | &nbsp;
+    a.underline(href="https://google.com/chrome") Chrome
+    | ,&nbsp;
+    a.underline(href="https://apple.com/safari") Safari
+    |, or&nbsp;
+    a.underline(href="https://firefox.com/") FireFox
 </template>
 
 <script>
