cd35989fd13da1eb84ef7b020b98ca9e8d6fc70e
acidjazz <acidjazz@gmail.com>
2019-08-19 22:29:04 -0500

    ✨ new component for the home page NationalForecast

---
+++ components/home/NationalForecast.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+OffsetSection(bg="supernova",fg="black")
+  .font-os.my-4(v-in-vp).vp-r  NATIONAL DATA
+  .text-4xl.text-bolder(v-in-vp).vp-r
+    | The country needs to build 328,000 new apartment homes each year to meet demand.
+  .mt-4.w-12.h-1.bg-white(v-in-vp).vp-r
+  .mt-4.text-bolder(v-in-vp).vp-r Forecasted by the year 2030
+</template>
+
+<script>
+import OffsetSection from '@/components/global/OffsetSection'
+import count from '@/mixins/count'
+export default {
+  components: { OffsetSection },
+  mixins: [ count ],
+  data () {
+    return {
+      build: {
+        endVal: 328000,
+        options: {
+          decimalPlaces: 0,
+          duration: 4,
+        }
+      },
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,8 @@
 .page.page-home.bg-black
   HomeHero
   LatestArticles(:feed="feed")
-  RenterGrowth
+  // RenterGrowth
+  NationalForecast
   FactTicker(range="CopyTickerHome")
   OurInitiative
   SourceLine
@@ -12,12 +13,12 @@
 import feed from '@/data/feed.json'
 import HomeHero from '@/components/home/HomeHero'
 import LatestArticles from '@/components/articles/LatestArticles'
-import RenterGrowth from '@/components/home/RenterGrowth'
+import NationalForecast from '@/components/home/NationalForecast'
 import FactTicker from '@/components/global/FactTicker'
 import OurInitiative from '@/components/home/OurInitiative'
 import SourceLine from '@/components/global/SourceLine'
 export default {
-  components: { HomeHero, LatestArticles, RenterGrowth, FactTicker, OurInitiative, SourceLine },
+  components: { HomeHero, LatestArticles, NationalForecast, FactTicker, OurInitiative, SourceLine },
   data () { return { feed: feed.payload.posts.slice(0, 3), } },
 }
 </script>
