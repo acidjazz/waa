ef8e672a41549ac71b9cc6fa7ad3d229cf68e2b9
acidjazz <acidjazz@gmail.com>
2019-08-20 10:46:48 -0500

    🎨 trying out fix for vision image on the home page

--- components/home/OurInitiative.vue
+++ components/home/OurInitiative.vue
@@ -2,7 +2,7 @@
 #OurInitiative
   OffsetSection
     .flex.flex-col.lg_flex-row(v-in-vp).vp-r.vp-d-3
-      img(src="/pathway.jpg",alt="pathway").w-full.lg_w-3_4
+      img(src="/pathway.jpg",alt="pathway").w-full.lg_w-3_4.h-64.lg_h-128
       .lg_ml-10.my-10.lg_my-0.flex.items-center(v-in-vp).vp-r.vp-d-4
         .copy
           .text-3xl.font-bold.mb-4 Our Vision
