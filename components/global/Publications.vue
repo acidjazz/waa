4d8a51d8164412ee174ccd4364041ed672dc82e6
kevin olson <acidjazz@gmail.com>
2019-08-23 16:29:21 -0500

    🔧 Future Demand Report

--- components/global/Publications.vue
+++ components/global/Publications.vue
@@ -9,7 +9,7 @@ OffsetSection(bg="supernova",fg="black")
       .my-8.inline-block.border.border-black.rounded-full.px-6.py-2
         a(href="/Economic_Impact.pdf") Download PDF
     .lg_w-1_3(v-in-vp).vp-r.vp-d-1
-      .text-2xl.text-bolder.mb-4 Future demand report
+      .text-2xl.text-bolder.mb-4 Future Demand Report
       .text-sm.pr-4.lg_h-24 NMHC and NAA partnered with Hoyt Advisory Services to identify the demand for apartments through 2030. Download their analysis of the apartment industry.
       .my-8.inline-block.border.border-black.rounded-full.px-6.py-2
         a(href="/NMHC-NAA-US-Apartment-Demand-in-2030.pdf") Download PDF
