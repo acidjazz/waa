1bc0eb7dc280ec10b5319b449a42ae83c73b97bf
kevin olson <acidjazz@gmail.com>
2020-03-10 21:46:37 -0500

    🔧 twice to three

--- components/vision/VisionHero.vue
+++ components/vision/VisionHero.vue
@@ -2,7 +2,7 @@
 SectionHero(type="image",src="/vision.jpg",gradient="bg-trandaisy")
   .text-sl.font-os.mb-10(v-in-vp).vp-y APARTMENT DEMAND
   .mx-auto.text-xl.lg_text-4xl.w-10_12.lg_w-192.mb-10(v-in-vp).vp-y.vp-d-2
-    p.mb-8 We need to build 328,000 new apartment homes each year to meet demand, a number we’ve only hit twice since 1989.
+    p.mb-8 We need to build 328,000 new apartment homes each year to meet demand, a number we’ve only hit three times since 1989.
     p The apartment industry stands ready to work with urban, suburban and rural communities in every region to meet the housing demand of Americans across all income levels.
 </template>
 
