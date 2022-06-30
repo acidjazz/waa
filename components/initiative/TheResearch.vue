427df741207a7b84e25d97436b9c5bff1fbe38ab
kevin olson <acidjazz@gmail.com>
2022-06-30 12:53:24 -0700

    💬 the research - copy updates - fixes #190

--- components/initiative/TheResearch.vue
+++ components/initiative/TheResearch.vue
@@ -4,8 +4,10 @@
     <div class="flex flex-col lg:flex-row lg:flex-wrap -mx-4">
       <div v-in-vp class="text-3xl text-bolder w-full lg:w-3/4 mb-8 vp-r vp-d-1">We partnered with top researchers at Hoyt Advisory Services and Eigen10 Advisors to estimate the economic impact of the apartment industry as well as demand for apartments in the future.</div>
       <div v-in-vp class="w-full lg:w-1/3 p-4 vp-r vp-d-2">
-        <div class="text-4xl font-okib font-bold h-28">Forecasted Demographic</div>
-        <div class="mt-4">Economic and demographic trend and forecast data was drawn from Moody’s Analytics® and supplemented by other sources including U.S. Census Bureau, Federal Reserve and other forecast surveys such as the Wall Street Journal Economic Forecasting Survey and the Federal Reserve Bank of Philadelphia Survey of Professional Forecasters. Property market data was derived from several sources including the U.S. Census Bureau, CoStar® Realty Information, CBRE® Econometrics and ESRI®.</div>
+        <div class="text-4xl font-okib font-bold h-28">Forecasted Demographic Data</div>
+        <div class="mt-4">
+          Demographic data was drawn from several U.S. Census Bureau surveys, including the 2020 American Community Survey (ACS) which was the most recent ACS survey at the time of this report. Economic and demographic trend and forecast data was drawn from Moody's Analytics supplemented by other sources including U.S. Census Bureau, Federal Reserve and other forecast surveys such as the Federal Reserve Bank of Philadelphia Survey of Professional Forecasters. Property market data was derived from the U.S. Census Bureau and CoStar Realty information.
+        </div>
       </div>
       <div v-in-vp class="w-full lg:w-1/3 p-4 vp-r vp-d-3">
         <div class="text-4xl font-okib font-bold h-28">Economic Impact Data</div>
