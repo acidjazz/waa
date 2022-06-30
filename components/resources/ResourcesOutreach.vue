df531d8382214c00acde86fb932095d113320a84
kevin olson <acidjazz@gmail.com>
2022-06-30 12:51:03 -0700

    ⏪ remove image and copy for outreach - fixes #189

--- components/resources/ResourcesOutreach.vue
+++ components/resources/ResourcesOutreach.vue
@@ -1,8 +1,8 @@
 <template>
   <offset-section>
     <div class="flex mb-12 flex-col lg:flex-row-reverse">
-      <div v-in-vp class="lg:w-2/3 vp-r vp-d-1"><img src="/pathway.jpg" alt="Pathway"></div>
-      <div v-in-vp class="lg:w-1/3 lg:mr-12 vp-r">
+      <!--<div v-in-vp class="lg:w-2/3 vp-r vp-d-1"><img src="/pathway.jpg" alt="Pathway"></div>-->
+      <div v-in-vp class="vp-r">
         <div class="font-os my-4">OUTREACH</div>
         <div class="text-4xl font-bold mb-2">We can do this</div>
         <div class="text-2xl font-bold mb-4">It takes a team</div>
@@ -11,6 +11,7 @@
         <div class="mb-4">By working together we can solve housing affordability challenges.</div>
       </div>
     </div>
+    <!--
     <div class="flex flex-col lg:flex-row">
       <div v-in-vp class="lg:w-1/2 lg:pr-8 vp-r vp-d-1">
         <div class="text-3xl font-bold mb-4 lg:h-28">Partnerships and Collaboration</div>
@@ -21,6 +22,7 @@
         <div class="mb-5">The Vision 2030 program outlines how public-private partnerships can help us solve the country's housing affordability challenge.</div><a class="waa-button-black-small mb-8" href="/Vision2030.pdf">Download PDF</a>
       </div>
     </div>
+    -->
   </offset-section>
 </template>
 
