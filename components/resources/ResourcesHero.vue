c817bb80396fa1f89ee31ba1502ce01deb9e97ab
kevin olson <acidjazz@gmail.com>
2022-06-30 11:59:34 -0700

    💬 copy changes for resources hero section - closes #186

--- components/resources/ResourcesHero.vue
+++ components/resources/ResourcesHero.vue
@@ -2,8 +2,12 @@
   <section-hero type="image" src="/vision.jpg" gradient="bg-trandaisy">
     <div v-in-vp class="text-sl font-os mb-10 vp-y">APARTMENT DEMAND</div>
     <div v-in-vp class="mx-auto text-xl lg:text-4xl w-10/12 lg:max-w-5xl mb-10 vp-y vp-d-2">
-      <p class="mb-8">We need to build 328,000 new apartment homes each year to meet demand, a number we’ve only hit three times since 1989.</p>
-      <p>The apartment industry stands ready to work with urban, suburban and rural communities in every region to meet the housing demand of Americans across all income levels.</p>
+      <p class="mb-8">
+        The U.S. needs to build 4.3 million more apartments by 2035 to meet the demand for rental housing. This includes 600,000 units (total) to fill the shortage from underbuilding after the 2008 financial crisis.
+      </p>
+      <p>
+        The apartment industry stands ready to work with urban, suburban and rural communities in every region to meet the housing demand of Americans across all income levels.
+      </p>
     </div>
   </section-hero>
 </template>
