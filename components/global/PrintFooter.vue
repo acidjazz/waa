96edb6614bc89b2cdc9d3172efe6f64730a59c26
kevin olson <acidjazz@gmail.com>
2022-07-01 10:16:20 -0700

    💬 fix print source line - fixes #201

--- components/global/PrintFooter.vue
+++ components/global/PrintFooter.vue
@@ -3,6 +3,8 @@
     <div class="flex flex-row justify-end">
       <img class="object-contain h-8 mr-4 filter-gray" src="/icon-nmhc.png" alt="NMHC Logo"><img class="object-contain h-8 filter-gray" src="/icon-naa.png" alt="NAA Logo">
     </div>
-    <div class="text-xs">Source: Hoyt Advisory Services; NMHC/NAA; U.S. Census Bureau</div>
+    <div class="text-xs">
+      Source: Hoyt Advisory Services; NMHC/NAA; 2020 American Community Survey, U.S. Census Bureau
+    </div>
   </div>
 </template>
