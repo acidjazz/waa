28713dc1e23f48a9196e09e1a27fbb644925363f
kevin olson <acidjazz@gmail.com>
2022-07-05 11:08:21 -0700

    ✏️  Quick edit to the Source line on the website - add "2020" before American Community Survey Fixes #205

--- components/global/SourceLine.vue
+++ components/global/SourceLine.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="canister -mt-16">
     <div v-in-vp class="text-white text-xs vp-y ml-10 lg_ml-0">
-      <span>Source:</span>
       <a href="http://hoytgroup.org/hoyt-advisory-services/" target="_new">Hoyt Advisory Services</a>;
       <a href="https://www.nmhc.org/" target="_new">NMHC</a>
       <span>/</span>
       <a href="http://www.naahq.org/" target="_new">NAA</a>;
-      <a href="https://www.census.gov/programs-surveys/acs/" target="_new">American Community Survey, U.S. Census Bureau</a>
+      <a href="https://www.census.gov/programs-surveys/acs/" target="_new">2020 American Community Survey, U.S. Census Bureau</a>
     </div>
   </div>
 </template>
