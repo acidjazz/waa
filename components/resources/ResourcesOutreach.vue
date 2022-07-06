31590a02e77db8f09ec5bd080f2f6e55c38b6115
kevin olson <acidjazz@gmail.com>
2022-07-06 11:25:30 -0700

    🎨 first stab and resources refactoring - addresses #203

--- components/resources/ResourcesOutreach.vue
+++ components/resources/ResourcesOutreach.vue
@@ -1,8 +1,10 @@
 <template>
   <offset-section>
     <div class="flex mb-12 flex-col lg:flex-row-reverse">
-      <!--<div v-in-vp class="lg:w-2/3 vp-r vp-d-1"><img src="/pathway.jpg" alt="Pathway"></div>-->
-      <div v-in-vp class="vp-r">
+      <div v-in-vp class="lg:w-2/3 vp-r vp-d-1">
+        <img src="/pathway.jpg" alt="Pathway">
+      </div>
+      <div v-in-vp class="lg:w-1/3 lg:mr-12 vp-r">
         <div class="font-os my-4">OUTREACH</div>
         <div class="text-4xl font-bold mb-2">We can do this</div>
         <div class="text-2xl font-bold mb-4">It takes a team</div>
@@ -11,6 +13,36 @@
         <div class="mb-4">By working together we can solve housing affordability challenges.</div>
       </div>
     </div>
+    <div class="flex flex-col lg:flex-row my-8">
+      <div v-in-vp class="lg:w-1/3 vp-r">
+        <div class="text-2xl text-bolder mb-4">Economic Impact Report</div>
+        <div class="text-sm pr-4 lg:h-24">
+          Hoyt Advisory Services and Eigen10 Advisors research estimates the estimated how much apartments and their residents contribute to the economy at the national, state and local level.
+        </div>
+        <div class="my-8 inline-block border border-black rounded-full px-6 py-2"><a href="/Economic_Impact.pdf">Download PDF</a></div>
+      </div>
+      <div v-in-vp class="lg:w-1/3 vp-r vp-d-1">
+        <div class="text-2xl text-bolder mb-4">Future Demand Research</div>
+        <div class="text-sm pr-4 lg:h-24">
+          NMHC and NAA partnered with Hoyt Advisory Services to identify the demand for apartments through 2035. Download their analysis of the apartment industry.
+        </div>
+        <ul class="list-disc text-sm ml-6">
+          <li><a href="#" class="text-heart">Download Executive Summary</a></li>
+          <li><a href="#" class="text-heart">Download National Report</a></li>
+          <li><a href="#" class="text-heart">Download 50-Markets Report</a></li>
+          <li><a href="#" class="text-heart">Download the full Report</a></li>
+        </ul>
+        <div class="my-8 inline-block border border-black rounded-full px-6 py-2"><a href="/NMHC-NAA-US-Apartment-Demand-in-2030.pdf">Download PDF</a></div>
+      </div>
+      <div v-in-vp class="lg:w-1/3 vp-r vp-d-2">
+        <div class="text-2xl text-bolder mb-4">Policies that work</div>
+        <div class="text-sm pr-4 lg:h-24">
+          Solutions to help build the housing the nation needs are out there but need to be more broadly adopted across the country. They include reducing barriers to apartment construction, reforming land-use and zoning regulations, reducing parking requirements, and creating more public-private partnerships.
+        </div>
+        <div class="my-8 inline-block border border-black rounded-full px-6 py-2"><a href="/Vision2030.pdf">Download PDF</a></div>
+      </div>
+    </div>
+
     <!--
     <div class="flex flex-col lg:flex-row">
       <div v-in-vp class="lg:w-1/2 lg:pr-8 vp-r vp-d-1">
