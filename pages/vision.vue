f05801a7a67651878f6064246c564f7e76d35108
kevin olson <acidjazz@gmail.com>
2022-07-28 17:34:25 -0500

    ♻️ redirecting vision page to resources page - closes #225

---
+++ pages/vision.vue
@@ -0,0 +1,7 @@
+<script>
+export default { mounted () { this.$router.push('/resources') } }
+</script>
+
+<template>
+  <div class="flex items-center justify-center text-xl">redirecting...</div>
+</template>
