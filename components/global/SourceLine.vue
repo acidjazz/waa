a0c793b848ed0f2f7bd27cf2c9bf7d5d8955ad0d
kevin olson <acidjazz@gmail.com>
2023-02-24 13:34:10 -0600

    🔧 2020 to 2021

--- components/global/SourceLine.vue
+++ components/global/SourceLine.vue
@@ -5,7 +5,7 @@
       <a href="https://www.nmhc.org/" target="_new">NMHC</a>
       <span>/</span>
       <a href="http://www.naahq.org/" target="_new">NAA</a>;
-      <a href="https://www.census.gov/programs-surveys/acs/" target="_new">2020 American Community Survey, U.S. Census Bureau</a>
+      <a href="https://www.census.gov/programs-surveys/acs/" target="_new">2021 American Community Survey, U.S. Census Bureau</a>
     </div>
   </div>
 </template>
